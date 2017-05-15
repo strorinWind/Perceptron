728530d06010fe987213944f7b42743bb0294fcb
StrorinWinder <StrorinWinder@DESKTOP-N2V8IFP>
2017-05-15 14:46:05 +0300

    Some design

--- Perceptron.UI/MainWindow.xaml.cs
+++ Perceptron.UI/MainWindow.xaml.cs
@@ -23,6 +23,24 @@ namespace Perceptron.UI
         public MainWindow()
         {
             InitializeComponent();
+            DrawPoints();
+        }
+
+        private void DrawPoints()
+        {
+            var cns = PointField;
+            for (int i = 0; i < 10; i++)
+            {
+                var rect = new Rectangle();
+                rect.Width = 5;
+                rect.Height = 5;
+                rect.Stroke = new SolidColorBrush(Colors.White);
+                rect.Fill = new SolidColorBrush(Colors.White);
+                Canvas.SetLeft(rect, 50+i*20);
+                Canvas.SetTop(rect, 70);
+                cns.Children.Add(rect);
+
+            }
         }
     }
 }
