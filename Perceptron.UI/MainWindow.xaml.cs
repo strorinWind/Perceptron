e61fa9203e99c7fcaaedef34db307aa29f3f475a
StrorinWinder <StrorinWinder@DESKTOP-N2V8IFP>
2017-06-05 11:16:59 +0300

    DrawPoints

--- Perceptron.UI/MainWindow.xaml.cs
+++ Perceptron.UI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,27 +21,44 @@ namespace Perceptron.UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Color[] colors = { Colors.Yellow, Colors.Blue, Colors.Red};
+
         public MainWindow()
         {
             InitializeComponent();
-            DrawPoints();
         }
 
         private void DrawPoints()
         {
             var cns = PointField;
-            for (int i = 0; i < 10; i++)
+            cns.Children.Clear();
+            cns.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+            cns.Arrange(new Rect(0, 0, this.Width, this.Height));
+            var width = cns.ActualWidth;
+            var height = cns.ActualHeight;
+
+            var m = File.ReadAllLines("../../N.points");
+            for (int i = 6; i < m.Length; i++)
             {
+                var a = m[i].Replace('.',',').Split();
+                var x = (double.Parse(a[0])+1)*width/3;
+                var y = (double.Parse(a[1])+1)*height/3;
+                var c = int.Parse(a[2])-1;
+
                 var rect = new Rectangle();
                 rect.Width = 5;
                 rect.Height = 5;
-                rect.Stroke = new SolidColorBrush(Colors.White);
-                rect.Fill = new SolidColorBrush(Colors.White);
-                Canvas.SetLeft(rect, 50+i*20);
-                Canvas.SetTop(rect, 70);
+                rect.Stroke = new SolidColorBrush(colors[c]);
+                rect.Fill = new SolidColorBrush(colors[c]);
+                Canvas.SetLeft(rect, x-2);
+                Canvas.SetTop(rect, y-2);
                 cns.Children.Add(rect);
-
             }
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {       
+            DrawPoints();
         }
     }
 }
